2d1fee41b72a2bfbc29d965925dffb2b7430c01b
magutierrezhijosa <magutierrezhijosa@gmail.com>
2024-11-11 15:44:00 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,31 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let indiceWho = Math.floor(Math.random() * who.length);
+  let indiceWhat = Math.floor(Math.random() * what.length);
+  let indiceWhen = Math.floor(Math.random() * when.length);
+  let indiceAction = Math.floor(Math.random() * action.length);
+
+  console.log(indiceWho);
+
+  document.querySelector("#excusa").innerHTML =
+    who[indiceWho] +
+    " " +
+    action[indiceAction] +
+    " " +
+    what[indiceWhat] +
+    " " +
+    when[indiceWhen];
 };
